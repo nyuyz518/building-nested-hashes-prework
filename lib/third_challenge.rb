28aed5a54f3430afd371a07ba566e08806a05243
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-27 02:21:19 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -19,7 +19,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet" ,
-        :age => 
+        :age => "50"
         },
       :matriarch => {
         :name => 
